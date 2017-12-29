8ea11209bcd10885c36c203fc643ce85d56fa835
Frank Kopp <frank@familie-kopp.de>
2017-12-29 12:54:40 +0100

    Version text

--- src/fko/pong/Pong.java
+++ src/fko/pong/Pong.java
@@ -32,7 +32,7 @@ import fko.pong.ui.PongUI;
 public class Pong {
 
 	// VERSION
-	public static final String VERSION = "0.1"; 
+	public static final String VERSION = "1.0"; 
 	
 	/**
 	 * The handle to the user interface class
